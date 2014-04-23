e9d41a06fdfa9959e5d6e953256d5fb277d2e261
DemetriousWilson <DemetriousWilson@gmail.com>
2014-04-22 17:06:04 -0700

    Create .gitignore Add new file ZigZagConversion.cpp

---
+++ ZigZagConversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	string convert(string s, int nRows) {
+		string answer = "";
+		if(0 == s.length() || nRows < 1) {
+			return "";
+		}
+		return answer;
+	}
+};
+
+int main() {
+	auto s = new Solution();
+	return 0;
+}
